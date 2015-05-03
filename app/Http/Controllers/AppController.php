2d00082950acc792b94c8e6d6af1de51f8600277
exile1205 <exile1205halladay@gmail.com>
2015-05-03 11:01:04 +0800

    fix comment

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -214,7 +214,8 @@ class AppController extends Controller {
 											->select('user__app__comments.id','users.name as user_name','users.id as user_id','users.img as user_img','apps.name as app_name','apps.id as app_id','apps.img_url as app_img','comment','user__app__comments.created_at')
 											->orderBy('user__app__comments.created_at','desc')
 											->where('user__app__comments.a_id','=',$id);
-			if(empty($comment_new->first())){
+			$empty_test = $comment_new->first();
+			if(empty($empty_test)){
 				return Response::json(array('message' => 'No comments', 'status' => 'error'));
 			}
 
